appendto/jquery-tmpl

commit 718c50e576cca22bcf682c664c226669411c795d

Changed tag-matching regex to respect the contents of strings.  (This fixes the tags-in-strings test cases.)

jquery.tmpl.js
@@ -140,7 +140,7 @@
 		    "_.index=$i||0;",
 		    "with($data){" // Introduce the data as local variables using with(){}
 		  ];
-		  while ( m = str.match( /^([\s\S]*?){{\s*(\/?)(\w+|\S)(?:\s+([\s\S]*?))?(?:\s+as\s+(.*?))?\s*}}/ ) ) {
+		  while ( m = str.match( /^([\s\S]*?){{\s*(\/?)(\w+|\S)(?:\s+((?:[^'"]*?|"[^"\\]*(?:\\.[^"\\]*)*"|'[^'\\]*(?:\\.[^'\\]*)*')*?))?(?:\s+as\s+(.*?))?\s*}}/ ) ) {
 		    
 		    // have prefix before tag
 		    if ( m[1] ) {

commit 5b53dd3f5cfef531f15c7912e9ed1429f24e3dc2

commit 718c50e576cca22bcf682c664c226669411c795d caused issues with IE. the intended fix was for curly braces within a tag body. reverting causes two tests to fail, but are fringe cases. will circle back for a better solution.

jquery.tmpl.js
@@ -145,8 +145,10 @@
 				"_.index=$i||0;",
 				"with($data){" // Introduce the data as local variables using with(){}
 			];
-			while ( m = str.match( /^([\s\S]*?){{\s*(\/?)(\w+|\S)(?:\s+((?:[^'"]*?|"[^"\\]*(?:\\.[^"\\]*)*"|'[^'\\]*(?:\\.[^'\\]*)*')*?))?(?:\s+as\s+(.*?))?\s*}}/ ) ) {
-				
+			//the regex from commit 718c50e576cca22bcf682c664c226669411c795d was breaking IE something awful.
+			//while ( m = str.match( /^([\s\S]*?){{\s*(\/?)(\w+|\S)(?:\s+((?:[^'"]*?|"[^"\\]*(?:\\.[^"\\]*)*"|'[^'\\]*(?:\\.[^'\\]*)*')*?))?(?:\s+as\s+(.*?))?\s*}}/ ) ) {
+			while ( m = str.match( /^([\s\S]*?){{\s*(\/?)(\w+|\S)(?:\s+([\s\S]*?))?(?:\s+as\s+(.*?))?\s*}}/ ) ) {
+			
 				// have prefix before tag
 				if ( m[1] ) {
 					s.push( "_.push('" + m[1].replace( rx_esc, fn_esc ) + "');" );

Tests/jquery.tmpl.qunit.js
@@ -34,13 +34,13 @@ function test_handler( test_name, res, exp ) {
 	}
 }
 
-
 // these are used throughout to test if tag blocks suppress them
 jQuery.tmplcmd.syntax_error		 = { prefix: "throw SyntaxError('test syntax error');" };
 jQuery.tmplcmd.reference_error = { prefix: "throw ReferenceError('test reference error');" };
 jQuery.tmplcmd.type_error			 = { prefix: "throw TypeError('test type error');" };
 
 module("Basics");
+
 
 	test("Basic Function", function() {
 
@@ -456,5 +456,4 @@ module("Script Tag Caching");
 		})(), true);		
 	
 	});
-		
-
+	